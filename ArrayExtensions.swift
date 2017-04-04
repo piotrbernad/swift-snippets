045721b64838814b47443202f26a9a2b5c747115
Piotr Bernad <piotr.bernad@appunite.com>
2017-04-04 18:54:51 +0200

    Array Extensions like unique, categorise

---
+++ ArrayExtensions.swift
@@ -0,0 +1,16 @@
+public extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        return Array(Set(self))
+    }
+}
+
+public extension Sequence {
+    func categorise<U : Hashable>(_ key: (Iterator.Element) -> U) -> [U:[Iterator.Element]] {
+        var dict: [U:[Iterator.Element]] = [:]
+        for el in self {
+            let key = key(el)
+            if case nil = dict[key]?.append(el) { dict[key] = [el] }
+        }
+        return dict
+    }
+}
